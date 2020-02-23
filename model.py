a0e091a93d8e0dcc32923d59fe10df435d33bd53
Farid Saïd <farid.said@gottexbrokers.com>
2020-02-23 19:44:23 +0100

    modification fichier model.py et delete de agents-100.zip

--- model.py
+++ model.py
@@ -1,9 +1,14 @@
-class Agent:
-	def say_Hello(self, first_name):
-		return "Bien le bonjour " + first_name
+import json
 
-	def __init__(self, agreeableness):
-		self.agreeableness = agreeableness
-		
-agent = Agent(0)
-print(agent.agreeableness)
+class Agent:
+    
+    def __init__(self, **agent_attributes):
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self, attr_name, attr_value)
+
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        agent = Agent(**agent_attributes)
+        print(agent.agreeableness)
+
+main()
